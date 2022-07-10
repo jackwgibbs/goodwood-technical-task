1c1f0ee9f24858dd3a8579d51e3a302a77f41471
Jack Gibbs <jackgibbs66@icloud.com>
2022-07-10 12:22:05 +0100

    Styling adjustments to filter and add products

--- Task 2/script.js
+++ Task 2/script.js
@@ -2,12 +2,56 @@ function radioFilter(day){
     //Function with parameter 'day'. It gets the radio button with corresponding day,
     //checks whether it is checked or unchecked, and makes it the opposite, i.e. checked
     //becomes unchecked. 
-    
+
     var button = document.getElementById(day);
     var checked = button.checked;
     if (checked == false){
         button.checked = true;
     }else{
         button.checked = false;
+    }
+
+    if (day == "tuesday"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "#cef2e8";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "white";
+        document.getElementById("thursdaydiv").style.backgroundColor= "white";
+        document.getElementById("fridaydiv").style.backgroundColor= "white";
+        document.getElementById("saturdaydiv").style.backgroundColor= "white";
+        document.getElementById("alldiv").style.backgroundColor= "white";
+    }else if (day == "wednesday"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "white";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "#cef2e8";
+        document.getElementById("thursdaydiv").style.backgroundColor= "white";
+        document.getElementById("fridaydiv").style.backgroundColor= "white";
+        document.getElementById("saturdaydiv").style.backgroundColor= "white";
+        document.getElementById("alldiv").style.backgroundColor= "white";
+    }else if (day == "thursday"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "white";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "white"
+        document.getElementById("thursdaydiv").style.backgroundColor= "#cef2e8";
+        document.getElementById("fridaydiv").style.backgroundColor= "white";
+        document.getElementById("saturdaydiv").style.backgroundColor= "white";
+        document.getElementById("alldiv").style.backgroundColor= "white";
+    }else if (day == "friday"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "white";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "white";
+        document.getElementById("thursdaydiv").style.backgroundColor= "white";
+        document.getElementById("fridaydiv").style.backgroundColor= "#cef2e8";
+        document.getElementById("saturdaydiv").style.backgroundColor= "white";
+        document.getElementById("alldiv").style.backgroundColor= "white";
+    }else if (day == "saturday"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "white";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "white";
+        document.getElementById("thursdaydiv").style.backgroundColor= "white";
+        document.getElementById("fridaydiv").style.backgroundColor= "white";
+        document.getElementById("saturdaydiv").style.backgroundColor= "#cef2e8";
+        document.getElementById("alldiv").style.backgroundColor= "white";
+    }else if (day == "all"){
+        document.getElementById("tuesdaydiv").style.backgroundColor= "white";
+        document.getElementById("wednesdaydiv").style.backgroundColor= "white";
+        document.getElementById("thursdaydiv").style.backgroundColor= "white";
+        document.getElementById("fridaydiv").style.backgroundColor= "white";
+        document.getElementById("saturdaydiv").style.backgroundColor= "white";
+        document.getElementById("alldiv").style.backgroundColor= "#cef2e8";
     }
 }
