0faa3fbe988eaf6047f3cb4827c5a42ac2cc9269
Jack Gibbs <jackgibbs66@icloud.com>
2022-07-10 11:37:12 +0100

    Improved radio selection buttons

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,3 +1,13 @@
-function filter(day){
-   alert(day);
+function radioFilter(day){
+    //Function with parameter 'day'. It gets the radio button with corresponding day,
+    //checks whether it is checked or unchecked, and makes it the opposite, i.e. checked
+    //becomes unchecked. 
+    
+    var button = document.getElementById(day);
+    var checked = button.checked;
+    if (checked == false){
+        button.checked = true;
+    }else{
+        button.checked = false;
+    }
 }
